da5a1b3cfc1d35e09b6c31dbbf8ad32b0b34ae82
assareh <assareh@me.com>
2021-05-18 13:18:21 -0700

    removed debug output

--- terraform-consul-config/outputs.tf
+++ terraform-consul-config/outputs.tf
@@ -7,11 +7,3 @@ output "web-token" {
   sensitive = true
   value     = consul_acl_token.web.id
 }
-
-output "url" {
-  value = trimprefix(data.hcs_cluster.default.consul_external_endpoint_url, "https://")
-}
-
-output "token" {
-  value = data.terraform_remote_state.hcs-cluster.outputs.consul_root_token_secret_id
-}
