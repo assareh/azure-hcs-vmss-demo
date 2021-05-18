177ff6d9c8375874ce9c90682fa65762e564b159
assareh <assareh@me.com>
2021-05-18 13:12:52 -0700

    another test output

--- terraform-consul-config/outputs.tf
+++ terraform-consul-config/outputs.tf
@@ -8,6 +8,10 @@ output "web-token" {
   value     = consul_acl_token.web.id
 }
 
+output "url" {
+  value = trimprefix(data.hcs_cluster.default.consul_external_endpoint_url, "https://")
+}
+
 output "token" {
   value = data.terraform_remote_state.hcs-cluster.outputs.consul_root_token_secret_id
 }
