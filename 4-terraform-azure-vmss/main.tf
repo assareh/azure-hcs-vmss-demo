d7696abc365adf5aefc6a8574def4eaeef32aaf2
assareh <assareh@me.com>
2021-05-19 15:31:36 -0700

    typo on admin username

--- 4-terraform-azure-vmss/main.tf
+++ 4-terraform-azure-vmss/main.tf
@@ -216,7 +216,7 @@ resource "azurerm_virtual_machine" "bastion_vm" {
 
     connection {
       type        = "ssh"
-      user        = var.admin_username
+      user        = "azureuser"
       private_key = tls_private_key.this.private_key_pem
       host        = azurerm_public_ip.bastion_ip.fqdn
     }
