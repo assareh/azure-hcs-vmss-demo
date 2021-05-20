58926034264e5447af4345284db7d2e646584083
assareh <assareh@me.com>
2021-05-19 19:20:55 -0700

    adding ansible to bastion

--- 4-terraform-azure-vmss/main.tf
+++ 4-terraform-azure-vmss/main.tf
@@ -211,7 +211,8 @@ resource "azurerm_virtual_machine" "bastion_vm" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get upgrade",
+      "sudo apt-get upgrade -y",
+      "sudo apt-get install -y ansible",
     ]
 
     connection {
