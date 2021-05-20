a79013012f62cc82328c8cb89f05561f3bf0231b
assareh <assareh@me.com>
2021-05-19 20:43:10 -0700

    adding "db"

--- 4-terraform-azure-vmss/main.tf
+++ 4-terraform-azure-vmss/main.tf
@@ -38,14 +38,14 @@ data "azurerm_subnet" "demo" {
   resource_group_name  = data.azurerm_resource_group.demo.name
 }
 
-data "template_file" "service" {
+data "template_file" "web" {
   template = file("${path.module}/templates/web-server.json")
   vars = {
     acl_token = data.terraform_remote_state.consul.outputs.web-token
   }
 }
 
-data "template_cloudinit_config" "this" {
+data "template_cloudinit_config" "web" {
   gzip          = true
   base64_encode = true
 
@@ -54,9 +54,9 @@ data "template_cloudinit_config" "this" {
     content_type = "text/cloud-config"
     content = templatefile("${path.module}/templates/vmss.yaml", {
       acl_token   = data.terraform_remote_state.consul.outputs.vm-token
-      consul_conf = data.hcs_cluster.default.consul_config_file
       ca_file     = data.hcs_cluster.default.consul_ca_file
-      web_service = base64encode(data.template_file.service.rendered)
+      consul_conf = data.hcs_cluster.default.consul_config_file
+      web_service = base64encode(data.template_file.web.rendered)
     })
   }
 }
@@ -71,7 +71,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "main" {
   resource_group_name             = data.azurerm_resource_group.demo.name
   location                        = data.azurerm_resource_group.demo.location
   sku                             = "Standard_B1s"
-  instances                       = 1
+  instances                       = 3
   admin_username                  = "azureuser"
   disable_password_authentication = true
 
@@ -82,7 +82,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "main" {
     public_key = tls_private_key.this.public_key_openssh
   }
 
-  custom_data = data.template_cloudinit_config.this.rendered
+  custom_data = data.template_cloudinit_config.web.rendered
 
   identity {
     type = "SystemAssigned"
@@ -102,6 +102,77 @@ resource "azurerm_linux_virtual_machine_scale_set" "main" {
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
+  }
+}
+
+resource "azurerm_network_interface" "db_nic" {
+  name                = "${var.prefix}-db-nic"
+  location            = data.azurerm_resource_group.demo.location
+  resource_group_name = data.azurerm_resource_group.demo.name
+  tags                = local.common_tags
+
+  ip_configuration {
+    name                          = "${var.prefix}-db-nic"
+    subnet_id                     = data.azurerm_subnet.demo.id
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+data "template_file" "db" {
+  template = file("${path.module}/templates/db.json")
+  vars = {
+    acl_token = data.terraform_remote_state.consul.outputs.db-token
+  }
+}
+
+data "template_cloudinit_config" "db" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "init.cfg"
+    content_type = "text/cloud-config"
+    content = templatefile("${path.module}/templates/db.yaml", {
+      acl_token   = data.terraform_remote_state.consul.outputs.vm-token
+      ca_file     = data.hcs_cluster.default.consul_ca_file
+      consul_conf = data.hcs_cluster.default.consul_config_file
+      db = base64encode(data.template_file.db.rendered)
+    })
+  }
+}
+
+resource "azurerm_virtual_machine" "db" {
+  name                          = "${var.prefix}-db-vm"
+  location                      = data.azurerm_resource_group.demo.location
+  resource_group_name           = data.azurerm_resource_group.demo.name
+  network_interface_ids         = [azurerm_network_interface.db_nic.id]
+  vm_size                       = "Standard_B1s"
+  delete_os_disk_on_termination = true
+  tags                          = local.common_tags
+
+  storage_os_disk {
+    name              = "OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    id = data.azurerm_image.web-server.id
+  }
+
+  os_profile {
+    admin_username = "azureuser"
+    computer_name  = "${var.prefix}-db-vm"
+    custom_data    = data.template_cloudinit_config.db.rendered
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/azureuser/.ssh/authorized_keys"
+      key_data = tls_private_key.this.public_key_openssh
+    }
   }
 }
 
@@ -136,13 +207,13 @@ resource "azurerm_network_security_group" "bastion_nsg" {
 }
 
 resource "azurerm_network_interface" "bastion_nic" {
-  name                = "${var.prefix}-nic"
+  name                = "${var.prefix}-bastion-nic"
   location            = data.azurerm_resource_group.demo.location
   resource_group_name = data.azurerm_resource_group.demo.name
   tags                = local.common_tags
 
   ip_configuration {
-    name                          = "${var.prefix}-nic"
+    name                          = "${var.prefix}-bastion-nic"
     subnet_id                     = data.azurerm_subnet.demo.id
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = azurerm_public_ip.bastion_ip.id
@@ -172,7 +243,7 @@ resource "azurerm_virtual_machine" "bastion_vm" {
   location                      = data.azurerm_resource_group.demo.location
   resource_group_name           = data.azurerm_resource_group.demo.name
   network_interface_ids         = [azurerm_network_interface.bastion_nic.id]
-  vm_size                       = "Standard_B2s"
+  vm_size                       = "Standard_B1s"
   delete_os_disk_on_termination = true
   tags                          = local.common_tags
 
