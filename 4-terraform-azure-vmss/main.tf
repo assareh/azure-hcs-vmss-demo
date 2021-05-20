c92b1ebd82ae3841887964863eb3f2507f41160c
assareh <assareh@me.com>
2021-05-19 20:57:28 -0700

    corrected example code

--- 4-terraform-azure-vmss/main.tf
+++ 4-terraform-azure-vmss/main.tf
@@ -115,16 +115,16 @@ resource "azurerm_linux_virtual_machine_scale_set" "main" {
 
 resource "azurerm_public_ip" "example" {
   name                = "${var.prefix}-pip"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
+  location            = data.azurerm_resource_group.demo.location
+  resource_group_name = data.azurerm_resource_group.demo.name
   allocation_method   = "Dynamic"
-  domain_name_label   = azurerm_resource_group.demo.name
+  domain_name_label   = data.azurerm_resource_group.demo.name
 }
 
 resource "azurerm_lb" "example" {
   name                = "${var.prefix}-lb"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
+  location            = data.azurerm_resource_group.demo.location
+  resource_group_name = data.azurerm_resource_group.demo.name
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
@@ -133,13 +133,13 @@ resource "azurerm_lb" "example" {
 }
 
 resource "azurerm_lb_backend_address_pool" "example" {
-  resource_group_name = azurerm_resource_group.demo.name
+  resource_group_name = data.azurerm_resource_group.demo.name
   loadbalancer_id     = azurerm_lb.example.id
   name                = "BackEndAddressPool"
 }
 
 resource "azurerm_lb_probe" "example" {
-  resource_group_name = azurerm_resource_group.demo.name
+  resource_group_name = data.azurerm_resource_group.demo.name
   loadbalancer_id     = azurerm_lb.example.id
   name                = "http-probe"
   protocol            = "Tcp"
@@ -147,7 +147,7 @@ resource "azurerm_lb_probe" "example" {
 }
 
 resource "azurerm_lb_nat_pool" "example" {
-  resource_group_name            = azurerm_resource_group.demo.name
+  resource_group_name            = data.azurerm_resource_group.demo.name
   name                           = "http"
   loadbalancer_id                = azurerm_lb.example.id
   protocol                       = "Tcp"
